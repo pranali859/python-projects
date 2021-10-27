5b8548583a8cc65c20a050ae4cc81e96ea4a9f42
pranali859 <83174167+pranali859@users.noreply.github.com>
2021-10-27 19:11:41 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,32 @@
+def add():
+    c = a + b
+    return c
+
+def sub():
+    d = a - b
+    return d
+
+def multi():
+    e = a*b
+    return e
+
+def div():
+    f = a/b
+    return f
+
+print("Please enter numbers below")
+a = int(input("Number 1:"))
+b = int(input("Number 2:"))
+o = input("operator:")
+
+if o=="+":
+    print(add())
+
+if o=="-":
+    print(sub())
+
+if o=="*":
+    print(multi())
+
+if o=="/":
+    print(div())
